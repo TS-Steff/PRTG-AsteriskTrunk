d014d2099c4ed2f364d01d1dc5c5ee0cdc77b5b6
TS-Steff <smueller@ts-management.ch>
2020-05-15 08:57:49 +0200

    Update monitor_trunk.sh

--- monitor_trunk.sh
+++ monitor_trunk.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-#value=$1
-value=$1
-var=$(/usr/sbin/asterisk -rx "sip show peers" | grep ${value} |awk '{print $6}'| sed 2,2d)
 
-if [[ $var == OK ]]; then
- echo "0:200:running"
-else
- echo "2:404:UNREACHABLE"
+value=$1
+serviceIsRunning=false
+
+status=$(/usr/sbin/asterisk -rx "sip show peers" |grep ${value}  |awk '{print $6}')
+
+if [ "$status" == OK ]; then
+        serviceIsRunning=true
+        echo "0:200:$status"
+fi
+
+if [ $serviceIsRunning == false ]; then
+        echo "2:404:$status"
 fi
